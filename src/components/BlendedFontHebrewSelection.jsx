aad8d4002243edd6746c31466af3d5f5b081a122
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2025-04-22 18:26:09 -0500

    remove unused code and unnecessary comments

--- src/components/BlendedFontHebrewSelection.jsx
+++ src/components/BlendedFontHebrewSelection.jsx
@@ -17,55 +17,21 @@ export default function BlendedFontHebrewSelection(blendedFontHebrewSelectionPro
     hebrewFontName,
     setHebrewFontName,
     webFontsHebrew,
-    // hebrewFfsId,
-    // setHebrewFfsId,
-    // hebrewFontDisplayName,
-    // setHebrewFontDisplayName,
-    // ffsArr,
     unicodeRanges,
     selectedFontClass,
     isHebrewDefault,
     handleClickHebrew,
   } = blendedFontHebrewSelectionProps;
 
-  /** There is not a possibility of Hebrew script or Greek script font-feature-settings the time of programming this page.
-   * // Available font font-feature-setting (Ffs) by selection
-   * const [hebrewFfsArr, setHebrewFfsArr] = useState([]);
-   *
-   * // Default settings (ideally the user's last settings, eventually, maybe...)
-   * const [hebrewFontSettings, setHebrewFontSettings] = useState([]);
-   *
-   * // Font-feature-settings CSS (string for application in css)
-   * const [hebrewFfsCss, setHebrewFfsCss] = useState('');
-   */
-
   // Example text
   const [exampleHebrew, setExampleHebrew] = useState('וַיֹּ֣אמֶר אֱלֹהִ֗ים יִקָּו֨וּ הַמַּ֜יִם מִתַּ֤חַת הַשָּׁמַ֙יִם֙ אֶל־מָק֣וֹם אֶחָ֔ד וְתֵרָאֶ֖ה הַיַּבָּשָׁ֑ה וַֽיְהִי־כֵֽן׃');
  
-  /** Set INITIAL options array (FfsArr), where applicable. */
-  /** Not yet applicable to Hebrew.
-   * useEffect(() => {
-   *    const selectedHebrewSettingsArr = ffsArr.filter(item => hebrewFfsId.includes(item.name));
-   *    setHebrewFfsArr(selectedHebrewSettingsArr);
-   *  },[ffsArr, hebrewFfsId]);
-   */
-
   /** Set SELECTED:
    *    - font class name substrings
-   *    - font display name (Not yet applicable to Hebrew.)
    *    - font name
-   *    - and where applicable (Not yet applicable to Hebrew.):
-   *        - font-feature-settings id (FfsId)
-   *        - options array (FfsArr)
    */
     const handleChangeHebrew = (event) => {
       setSelectedHebrewFontClassSubstr(event.target.value);
-      // const selectedHebrewSettingId = webFontsHebrew.filter(font => font.id === event.target.value).map((font, index) => (font.settings_id));
-      // setHebrewFfsId(selectedHebrewSettingId);
-      // const selectedHebrewSettingsId = ffsArr.filter(item => selectedHebrewSettingId.includes(item.name));
-      // setHebrewFfsArr(selectedHebrewSettingsId);
-      // const hebrewDisplayName = webFontsHebrew.filter(font => font.id === event.target.value).map((font, index) => (font.display_name));
-      // setHebrewFontDisplayName(hebrewDisplayName);
       const hebrewName = webFontsHebrew.filter(font => font.id === event.target.value).map((font, index) => (font.name));
       setHebrewFontName(hebrewName);
     };
@@ -81,22 +47,6 @@ export default function BlendedFontHebrewSelection(blendedFontHebrewSelectionPro
         <FontMenuItem font={font} {...fontMenuItemProps} />
       </MenuItem>
     ));
-  
-  /** Not yet applicable to Hebrew.
-   *  useEffect(() => {
-   *    if (hebrewFontSettings.length !== 0) {
-   *      const hebrewFfsJsx = hebrewFontSettings.map((obj, index) => (
-   *        <div key={index}> ~{obj.name}~ {obj.value},</div>
-   *      ));
-   *      // convert jsx return to string and remove html tags and attributes (e.g., div's)
-   *      const hebrewFfsStr = renderToString(hebrewFfsJsx).replace(/(<([^>]+)>)/ig, '').replace(/~/gm, '"');
-   *      // remove the last comma, change ~ to "
-   *      setHebrewFfsCss(hebrewFfsStr.substring(0, hebrewFfsStr.length - 1).replace(/~/gm, '"'));
-   *    } else {
-   *        setHebrewFfsCss("");
-   *    }
-   *  },[hebrewFontSettings, setHebrewFfsCss])
-   */
  
   const unicodeRangeHebrew = selectedHebrewFontClassSubstr === '' ? '' : unicodeRanges.filter(item => (item.name === 'Hebrew')).map((script, index) => (script.unicode_range));
   const neutralScope = ' ';
@@ -110,41 +60,9 @@ export default function BlendedFontHebrewSelection(blendedFontHebrewSelectionPro
   const hebrewFontSize = '200%';
   const hebrewLineHeight = '1.4';
 
-  /** Not yet applicable to Hebrew.
-   * const fontFeaturesHebrewProps = {
-   *  hebrewFontSettings, // [] then default then selected
-   *  setHebrewFontSettings,
-   *  ffsId: hebrewFfsId,
-   *  fontName: hebrewFontName,
-   *  fontDisplayName: hebrewFontDisplayName,
-   *  fontSize: hebrewFontSize,
-   *  lineHeight: hebrewLineHeight,
-   *  isGraphiteAssumed,
-   *  ffsArr: hebrewFfsArr,  // Options
-   *  exampleRegex: regexHebrew,
-   *  setExampleHebrew,
-   * };
-   *
-   *
-   * // Generate assumed filenames
-   * const hebrewFfsCssFilename = hebrewFontName.toString().replace(/^P/, 'p');
-   *
-   * const hebrewCss = (
-   *   <div>
-   *     <br />
-   *     <b><em>To update <em>{hebrewFontDisplayName}</em> font-feature-settings, set all instances of font-features-settings, -moz-font-feature-settings, and -webkit-font-feature-setting...:</em></b>
-   *     <ul>
-   *       <li> <b>in: </b><em>~/pankosmia_working/{hebrewFfsCssFilename}.css</em>' <b>to:</b> {hebrewFfsCss};</li>
-   *     </ul>
-   *   </div>
-   * );
-   */
-
   const exampleHebrewDir = useDetectDir({ text: exampleHebrew, isMarkup: false, ratioThreshold: .51 });
 
-  // const showHebrewFeatures = hebrewFfsArr.length > 0;
   const showHebrewTextArea = selectedHebrewFontClassSubstr.length !== 0;
-  // const showHebrewCss = hebrewFfsCss !== '' && selectedHebrewFontClassSubstr.length !== 0;
 
   return (
     <Grid2 container spacing={2}>
@@ -180,7 +98,6 @@ export default function BlendedFontHebrewSelection(blendedFontHebrewSelectionPro
                 <RestoreIcon sx={{ cursor: 'pointer' }} style={{ color: "purple", paddingLeft: '9px', margin: 'auto 0' }} onClick={handleClickHebrew} />
               </Tooltip>
             }
-            {/** showHebrewFeatures && <FontFeaturesHebrew {...fontFeaturesHebrewProps} /> */}
           </Stack>
         </div>
       </Grid2>
@@ -200,14 +117,10 @@ export default function BlendedFontHebrewSelection(blendedFontHebrewSelectionPro
                 width: '50%',
                 borderColor: "purple",
                 direction: exampleHebrewDir,
-                /** fontFeatureSettings: hebrewFfsCss !== '' && hebrewFfsCss,
-                MozFontFeatureSettings: hebrewFfsCss !== '' && hebrewFfsCss,
-                WebkitFontFeatureSettings: hebrewFfsCss !== '' && hebrewFfsCss, */
                 }}
               value={showHebrewTextArea && exampleHebrew}
             />
           }
-          {/** showHebrewCss ? hebrewCss : (<div><br /></div>) */}
         </Box>
       </Grid2>
       <br />
@@ -226,16 +139,6 @@ BlendedFontHebrewSelection.propTypes = {
   setHebrewFontName: PropTypes.func.isRequired,
   /** Available Hebrew Fonts */
   webFontsHebrew: PropTypes.array.isRequired,
-  /** Font-feature-settings (Ffs) id for settings lookup */
-  // hebrewFfsId: PropTypes.array.isRequired,
-  /** Set Font-feature-settings (Ffs) id for settings lookup */
-  // setHebrewFfsId: PropTypes.func.isRequired,
-  /** Font Display Name for font-feature-settings heading */
-  // hebrewFontDisplayName: PropTypes.array.isRequired,
-  /** Set Font Display Name for font-feature-settings heading and shortlist */
-  // setHebrewFontDisplayName: PropTypes.func.isRequired,
-  /** Font Feature Settings Array (Options)*/
-  // ffsArr: PropTypes.array.isRequired,
   /** Unicode ranges for RegEx by script type for editable examples */
   unicodeRanges: PropTypes.array.isRequired,
   /** Selected Font Class */
