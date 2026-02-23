af4185e658bcc1c9183624cfa3155e4347e40ab4
lmeline <loise13@hotmail.fr>
2026-02-23 10:43:12 +0100

    language drag and drop

--- src/components/LanguageSelection.jsx
+++ src/components/LanguageSelection.jsx
@@ -1,17 +1,18 @@
-import { Box, Button, FormControl, InputLabel, MenuItem, OutlinedInput, Select } from "@mui/material";
-import { doI18n } from "pithekos-lib";
+import { Box, FormControl, IconButton, List, ListItem, ListItemText, MenuItem, Select } from "@mui/material";
+import { doI18n, postEmptyJson } from "pithekos-lib";
 import { i18nContext } from "pankosmia-rcl";
 
 import { useState, useEffect, useContext } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import LanguageMenuItem from "./LanguageMenuItem";
+import { DragIndicator } from "@mui/icons-material";
 
-export default function LanguageSelection({ languageChoices, usedLanguages }) {
+export default function LanguageSelection({ languageChoices, usedLanguages, setLanguageChoices }) {
   const { i18nRef } = useContext(i18nContext)
   const [items, setItems] = useState([]);
   const [selectIsOpen, setSelectIsOpen] = useState();
-  const [selectedLanguage,setSelectedLanguage] = useState("");
-  console.log("langue",selectedLanguage)
+  const [selectedLanguage, setSelectedLanguage] = useState("");
+
   useEffect(() => {
     if (Array.isArray(languageChoices)) {
       setItems(
@@ -33,9 +34,21 @@ export default function LanguageSelection({ languageChoices, usedLanguages }) {
   const onDragEnd = (result) => {
     if (!result.destination) return;
 
-    setItems(reorder(items, result.source.index, result.destination.index));
-  };
+    const reorderedItems = reorder(
+      items,
+      result.source.index,
+      result.destination.index
+    );
 
+    setItems(reorderedItems);
+
+    const newOrder = reorderedItems.map(item => item.id);
+
+    setLanguageChoices(newOrder);
+
+    const languageString = newOrder.join("/");
+    postEmptyJson(`/settings/languages/${languageString}`);
+  };
   const grid = 8;
 
   const getItemStyle = (isDragging, draggableStyle) => ({
@@ -51,15 +64,32 @@ export default function LanguageSelection({ languageChoices, usedLanguages }) {
     padding: grid,
     width: 285
   });
-  const handleClick = (event) => {
-    setSelectIsOpen(true);
-  };
+
+  const doChange = (selected) => {
+    setLanguageChoices((prev) => {
+      let updated = [...prev];
+
+      if (!updated.includes(selected)) {
+        updated.push(selected);
+      }
+      if (!updated.includes("en")) {
+        updated.push("en");
+      }
+
+      const languageString = updated.join("/");
+      postEmptyJson(`/settings/languages/${languageString}`);
+
+      return updated;
+    });
+
+    setSelectedLanguage("");
+  }
   return (
     <Box sx={{ width: 300 }}>
       <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="droppable">
           {(provided, snapshot) => (
-            <div
+            <List
               {...provided.droppableProps}
               ref={provided.innerRef}
               style={getListStyle(snapshot.isDraggingOver)}
@@ -67,7 +97,7 @@ export default function LanguageSelection({ languageChoices, usedLanguages }) {
               {items.map((item, index) => (
                 <Draggable key={item.id} draggableId={item.id} index={index}>
                   {(provided, snapshot) => (
-                    <div
+                    <ListItem
                       ref={provided.innerRef}
                       {...provided.draggableProps}
                       {...provided.dragHandleProps}
@@ -75,33 +105,37 @@ export default function LanguageSelection({ languageChoices, usedLanguages }) {
                         snapshot.isDragging,
                         provided.draggableProps.style
                       )}
-                    >
-                      {item.content}
-                    </div>
+                      >
+                      <IconButton {...provided.dragHandleProps} size="small" sx={{cursor:"grab"}}>
+                        <DragIndicator/>
+                      </IconButton>
+                      <ListItemText primary={item.content}/>
+                    </ListItem>
                   )}
                 </Draggable>
               ))}
               {provided.placeholder}
-            </div>
+            </List>
           )}
         </Droppable>
-            <FormControl size="small" fullWidth>
-              <Select
-               label={doI18n("pages:core-settings:add", i18nRef.current)}
-               value={selectedLanguage || ""}
-                onClose={() => setSelectIsOpen(false)}
-                onChange={(e) => setSelectedLanguage(e.target.value)}
-              >
-                {usedLanguages
-                  .filter(item => !languageChoices.includes(item.id))
-                  .map((language) => (
-                    <MenuItem key={language.id} value={language.id} dense>
-                      <LanguageMenuItem language={language} />
-                    </MenuItem>
-                  ))
-                }
-              </Select>
-            </FormControl>
+        <FormControl size="small" fullWidth>
+
+          <Select
+            label={doI18n("pages:core-settings:add", i18nRef.current)}
+            value={selectedLanguage || ""}
+            onClose={() => setSelectIsOpen(false)}
+            onChange={(ev) => {doChange(ev.target.value)}}>
+            {usedLanguages
+
+              .filter(item => !languageChoices.includes(item.id))
+              .map((language) => (
+                <MenuItem key={language.id} value={language.id} dense>
+                  <LanguageMenuItem language={language} />
+                </MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
       </DragDropContext>
     </Box>
 
--- src/components/LanguageSelection1.jsx
+++ src/components/LanguageSelection1.jsx
@@ -1,170 +1,129 @@
-import { useContext, useRef, useState } from "react";
+import { useContext, useState } from "react";
 
-import { Grid2, Box, InputLabel, FormControl, Select, MenuItem, Stack, Button, Typography, ButtonGroup, Popper, Grow, Paper, ClickAwayListener, List, ListItemButton, ListItemIcon, ListItemText, Divider, Menu, MenuList } from "@mui/material";
+import { Grid2, Box, InputLabel, FormControl, Select, MenuItem, Stack, Button, Typography } from "@mui/material";
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 import { doI18n, postEmptyJson } from "pithekos-lib";
-import { i18nContext } from "pithekos-lib";
-import AddIcon from '@mui/icons-material/Add';
+import { i18nContext} from "pithekos-lib";
+
 import sx from "./Selection.styles";
 import LanguageMenuItem from "./LanguageMenuItem";
 
-const LanguageSelector = ({ usedLanguages, languageChoices, setLanguageChoices, doChange }) => {
-  const { i18nRef } = useContext(i18nContext);
-  const handleClick = (event) => {
-    setSelectIsOpen(true);
-  };
+const LanguageSelector = ({usedLanguages, languageChoices, setLanguageChoices, langN, selectedLanguage, doChange, selectIsOpen, setSelectIsOpen}) => {
+  const {i18nRef} = useContext(i18nContext);
+console.log("languagechoices",languageChoices)
+  return (
+        <div item style={{padding: "1.25em 0 0 0"}}>
+          <Box>
+            <Stack direction="row">
+              <FormControl size="small">
+                  <InputLabel id="select-language-label" htmlFor="select-language" sx={sx.inputLabel}>
+                      {doI18n("pages:core-settings:language", i18nRef.current)}{" "}{langN + 1}
+                  </InputLabel>
+                  <Select
+                      variant="outlined"
+                      labelId="select-language-label"
+                      open={selectIsOpen}
+                      onClose={() => setSelectIsOpen(false)}
+                      onOpen={() => setSelectIsOpen(true)}
+                      name="select-language"
+                      inputProps={{
+                          id: "select-language",
+                      }}
+                      value={selectedLanguage}
+                      label={doI18n("pages:core-settings:language", i18nRef.current)}
+                      onChange={ev => doChange(languageChoices, ev.target.value, langN, setLanguageChoices)}
+                      sx={sx.select}
+                  >
+                      {
+                          usedLanguages.filter(item => !languageChoices.slice(0, langN).includes(item.id)).map((language, n) => <MenuItem key={n} value={language.id} dense>
+                                  <LanguageMenuItem language={language}/>
+                              </MenuItem>
+                          )
+                      }
+                  </Select>
+              </FormControl>
+              <div style={{margin: '8px auto auto 4px'}}>
+                {selectedLanguage !== 'en' && <RemoveCircleIcon color="secondary" onClick={ev => doChange(languageChoices, selectedLanguage, langN, setLanguageChoices, langN)}  />}
+              </div>
+            </Stack>
+          </Box>
+        </div>
+  );
+}
+
+const LangSelectors = ({languageChoices, setLanguageChoices, usedLanguages, doChange}) => {
+
+  const {i18nRef} = useContext(i18nContext);
   const [selectIsOpen, setSelectIsOpen] = useState(false);
   let selectorSpecs = [];
   let usedHereLanguages = new Set([]);
   for (const [n, lang] of languageChoices.entries()) {
-    selectorSpecs.push(
-      {
-        langN: n,
-        selectedLanguage: lang,
-        usedLanguages: usedLanguages
-      }
-    );
-    usedHereLanguages.add(lang);
+      selectorSpecs.push(
+          {
+              n,
+              selectedLanguage: lang,
+              usedLanguages: usedLanguages
+          }
+      );
+      usedHereLanguages.add(lang);
   }
-  console.log("used",usedLanguages)
-  let langN = selectorSpecs.map(s => s.langN)
-  return (
-    <Paper sx={{ width: 320 }}>
-      <MenuList dense>
-        {languageChoices.map((lang, index) => (
-          <MenuItem key={index}>
-            <ListItemText inset>{index + 1} {lang}</ListItemText>
-          </MenuItem>
-        ))}
-        <Divider />
-        <MenuItem onClick={handleClick}>
-          <ListItemText inset>Add</ListItemText>
-        </MenuItem>
-        {selectIsOpen && (
-          <MenuItem>
-            <FormControl size="small" fullWidth>
-              <Select
-                value={'choose your language'}
-                onClose={() => setSelectIsOpen(false)}
-                onChange={ev => {
-                  doChange(languageChoices, ev.target.value, langN, setLanguageChoices);
-                }
-                }
-              >
-                {
-                  usedLanguages.filter(item => !languageChoices.slice(0, langN).includes(item.id)).map((language, n) => <MenuItem key={n} value={language.id} dense>
-                    <LanguageMenuItem language={language} />
-                  </MenuItem>
-                  )
-                }
-              </Select>
-            </FormControl>
-          </MenuItem>
-        )}
-      </MenuList>
-    </Paper>
-  )
-  // return (
-  //   <div item style={{ padding: "1.25em 0 0 0" }}>
-  //     <Box>
-  //       <Stack direction="row">
-  //         <FormControl size="small">
-  //           <InputLabel id="select-language-label" htmlFor="select-language" sx={sx.inputLabel}>
-  //             {doI18n("pages:core-settings:language", i18nRef.current)}{" "}{langN + 1}
-  //           </InputLabel>
-  //           <Select
-  //             variant="outlined"
-  //             labelId="select-language-label"
-  //             open={selectIsOpen}
-  //             onClose={() => setSelectIsOpen(false)}
-  //             onOpen={() => setSelectIsOpen(true)}
-  //             name="select-language"
-  //             inputProps={{
-  //               id: "select-language",
-  //             }}
-  //             value={selectedLanguage}
-  //             label={doI18n("pages:core-settings:language", i18nRef.current)}
-  //             onChange={ev => doChange(languageChoices, ev.target.value, langN, setLanguageChoices)}
-  //             sx={sx.select}
-  //           >
-  //             {
-  //               usedLanguages.filter(item => !languageChoices.slice(0, langN).includes(item.id)).map((language, n) => <MenuItem key={n} value={language.id} dense>
-  //                 <LanguageMenuItem language={language} />
-  //               </MenuItem>
-  //               )
-  //             }
-  //           </Select>
-  //         </FormControl>
-  //         <div style={{ margin: '8px auto auto 4px' }}>
-  //           {selectedLanguage !== 'en' && <RemoveCircleIcon color="secondary" onClick={ev => doChange(languageChoices, selectedLanguage, langN, setLanguageChoices, langN)} />}
-  //         </div>
-  //       </Stack>
-  //     </Box>
-  //   </div>
-  // );
-
+  return <div>
+      <Button variant='outlined' color='secondary' sx={{ display: 'block', mt: 2 }} onClick={() => setSelectIsOpen(true)}>
+        {doI18n("pages:core-settings:add", i18nRef.current)}
+      </Button>
+      {selectorSpecs.map((s, n) => <LanguageSelector
+          key={n}
+          langN={n}
+          selectedLanguage={s.selectedLanguage}
+          usedLanguages={s.usedLanguages}
+          languageChoices={languageChoices}
+          setLanguageChoices={setLanguageChoices}
+          doChange={doChange}
+          selectIsOpen={n === selectorSpecs.length - 1 ? selectIsOpen : undefined}
+          setSelectIsOpen={n === selectorSpecs.length - 1 ? setSelectIsOpen : undefined}/>)
+      }
+  </div>
 }
-
-// const LangSelectors = ({ languageChoices, setLanguageChoices, usedLanguages, doChange }) => {
-
-//   const { i18nRef } = useContext(i18nContext);
-
-//   return <div>
-//     {selectorSpecs.map((s, n) => <LanguageSelector
-//       key={n}
-//       langN={n}
-//       selectedLanguage={s.selectedLanguage}
-//       usedLanguages={s.usedLanguages}
-//       languageChoices={languageChoices}
-//       setLanguageChoices={setLanguageChoices}
-//       doChange={doChange}
-//       selectIsOpen={n === selectorSpecs.length - 1 ? selectIsOpen : undefined}
-//       setSelectIsOpen={n === selectorSpecs.length - 1 ? setSelectIsOpen : undefined} />)
-//     }
-//   </div>
-// }
 
 const doChange = (allLanguages, choice, langN, setLanguageChoices, remove) => {
   let newLanguages = [];
-
-  console.log("newLang", newLanguages)
   for (const [n, lang] of allLanguages.entries()) {
-    if (newLanguages.includes(lang)) {
-      continue;
-    }
-    if (n === langN) {
-      if (newLanguages.includes(choice)) {
-        continue;
+      if (newLanguages.includes(lang)) {
+          continue;
       }
-      if (n !== remove) {
-        newLanguages.push(choice);
+      if (n === langN) {
+          if (newLanguages.includes(choice)) {
+              continue;
+          }
+          if (n !== remove) {
+            newLanguages.push(choice);
+          }
+          if (choice ===  "en") {
+              // break; // No options after English
+              continue
+          }
+          continue;
       }
-      if (choice === "en") {
-        // break; // No options after English
-        continue
+      if (lang === "en") {
+          newLanguages.push("en");
+          // break; // No options after English
       }
-      continue;
-    }
-    if (lang === "en") {
-      newLanguages.push("en");
-      // break; // No options after English
-    }
-    if (newLanguages.includes(lang)) {
-      continue;
-    }
-    newLanguages.push(lang);
+      if (newLanguages.includes(lang)) {
+          continue;
+      }
+      newLanguages.push(lang);
   }
   // if (newLanguages[newLanguages.length - 1] !== "en") { // Always have English last
   if (!newLanguages.includes("en")) { // English must appear somewhere
-    newLanguages.push("en");
+       newLanguages.push("en");
   }
   const languageString = newLanguages.join('/');
   postEmptyJson(`/settings/languages/${languageString}`).then();
   setLanguageChoices(newLanguages);
 }
 
-export default function LanguageSelection2(languageSelectionProps) {
-  const { i18nRef } = useContext(i18nContext);
+export default function LanguageSelection(languageSelectionProps) {
+  const {i18nRef} = useContext(i18nContext);
   const {
     languageChoices,
     setLanguageChoices,
@@ -178,11 +137,11 @@ export default function LanguageSelection2(languageSelectionProps) {
         <Typography variant='body1' >{doI18n("pages:core-settings:user_interface_desc", i18nRef.current)}</Typography>
       </Grid2>
       <Grid2 size={12}>
-        <LanguageSelector
-          languageChoices={languageChoices}
-          setLanguageChoices={setLanguageChoices}
-          usedLanguages={usedLanguages}
-          doChange={doChange}
+        <LangSelectors
+            languageChoices={languageChoices}
+            setLanguageChoices={setLanguageChoices}
+            usedLanguages={usedLanguages}
+            doChange={doChange}
         />
       </Grid2>
     </Grid2>
